d5bfb938e3deb9cbd3ee3705b6b97b7b41304f5a
Gael Rs <gaelrszk@gmail.com>
2025-05-10 21:03:38 -0500

    feat: add register form provider component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import LoginFormComponent from "./iam/components/login-form/login-form.component.vue";
 import RegisterFormComponent from "./iam/components/register-form-client/register-form-client.component.vue";
+import RegisterFormProvider from "./iam/components/register-form-provider/register-form-provider.vue";
 </script>
 
 <template>
 
   <login-form-component/>
   <register-form-component/>
+  <register-form-provider/>
 
 </template>
 
---
+++ src/iam/components/register-form-provider/register-form-provider.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+  name: "register-form-provider"
+}
+</script>
+
+<template>
+  <form class="register-form">
+    <pv-card class="register-card">
+      <template #content>
+        <div class="full-width">
+          <label for="email">Email Address</label>
+          <input
+              id="email"
+              type="email"
+              v-model="email"
+              placeholder="example@business.com"
+              class="p-inputtext p-component"
+          />
+        </div>
+
+        <div class="full-width">
+          <label for="password">Password</label>
+          <input
+              id="password"
+              type="password"
+              v-model="password"
+              placeholder="Your password"
+              class="p-inputtext p-component"
+          />
+        </div>
+
+        <div class="full-width">
+          <label for="businessName">Business Name</label>
+          <input
+              id="businessName"
+              type="text"
+              v-model="businessName"
+              placeholder="My Business"
+              class="p-inputtext p-component"
+          />
+        </div>
+      </template>
+
+      <template #footer>
+        <pv-button
+            label="Register as a Provider"
+            class="register-button"
+            @click="registerProvider"
+        />
+      </template>
+    </pv-card>
+  </form>
+</template>
+
+<style scoped>
+.register-form {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.register-card {
+  width: 100%;
+  max-width: 400px;
+  padding: 16px;
+  background-color: rgb(255, 158, 158);
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.3);
+  border-radius: 12px;
+}
+
+.full-width {
+  width: 100%;
+  margin-bottom: 16px;
+  display: flex;
+  flex-direction: column;
+}
+
+.register-button {
+  width: 100%;
+  text-transform: uppercase;
+  background-color: #211f1f;
+  color: #ffffff;
+  border: none;
+}
+</style>
