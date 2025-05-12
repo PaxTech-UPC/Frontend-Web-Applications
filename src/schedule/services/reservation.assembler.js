0e8092b418571b17e252bf3de7d668ee495c8235
Gael Rs <gaelrszk@gmail.com>
2025-05-12 11:32:19 -0500

    fix: conflic problem

--- src/schedule/services/reservation.assembler.js
+++ src/schedule/services/reservation.assembler.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Reservation } from "../model/reservation.entity.js";
 
 export class ReservationAssembler {
@@ -10,18 +9,4 @@ export class ReservationAssembler {
         console.log(response.data.map(resource => this.toEntityFromResource(resource)));
         return response.data.map(resource => this.toEntityFromResource(resource));
     }
-=======
-import { Appointment} from "../../AppointmentsClient/model/Appointment.entity.js";
-
-export function assembleAppointments(rawAppointments) {
-    return rawAppointments.map(res => new Appointment({
-        reservationId: res.reservation.id,
-        tipo: res.tipo,
-        clientName: res.client.user.name,
-        salonName: res.salon.name,
-        paymentStatus: res.payment.status,
-        timeSlotStart: new Date(res.timeSlot.start),
-        timeSlotEnd: new Date(res.timeSlot.end)
-    }));
->>>>>>> feature/Appointment
 }
