3627b86d88502045a22a0e05fa2dc9107eb62a7a
aaaaangie <angieyalan17@gmail.com>
2025-05-14 13:59:14 -0500

    fix: fix subscription tab

--- src/subscription/components/subscription-plan/subscription-plan.component.vue
+++ src/subscription/components/subscription-plan/subscription-plan.component.vue
@@ -1,36 +1,52 @@
 <script>
+import { useToast } from 'primevue/usetoast';
+import { useConfirm } from 'primevue/useconfirm';
 
 export default {
   name: "subscription-plan-component",
-}
+  data() {
+    return {
+      selectedPlan: ''
+    };
+  },
+  methods: {
+    changePlan(plan) {
+      const toast = useToast();
+      const confirm = useConfirm();
 
-function changePlan(plan) {
-  confirm.require({
-    message: 'Are you sure you want to change your subscription plan? This action will update your benefits and take effect immediately.',
-    header: 'Confirmation',
-    icon: 'pi pi-exclamation-triangle',
-    acceptLabel: 'Yes',
-    rejectLabel: 'No',
-    accept: () => {
-      selectedPlan.value = plan;
-      toast.add({ severity: 'success', summary: 'Plan Updated', detail: `You have selected the ${plan} plan.`, life: 3000 });
+      confirm.require({
+        message: 'Are you sure you want to change your subscription plan? This action will update your benefits and take effect immediately.',
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Yes',
+        rejectLabel: 'No',
+        accept: () => {
+          this.selectedPlan = plan;
+          toast.add({
+            severity: 'success',
+            summary: 'Plan Updated',
+            detail: `You have selected the ${plan} plan.`,
+          });
+        }
+      });
     }
-  });
-}
+  }
+};
 </script>
 
 <template>
   <div>
-    <h2>
+    <h1></h1>
+    <h2 class="header">
       {{$t('subscription.currentPlan')}}
       <div class="plan-container">{{ selectedPlan || 'ProStyle' }}</div>
     </h2>
 
-    <h3>{{$t('subscription.upgrade')}}</h3>
+    <h3 class="subheader">{{$t('subscription.upgrade')}}</h3>
     <div class="plans">
       <div class="plan-card essential" @click="changePlan('Essential')">
-        <h4>Essential</h4>
-        <p>Free</p>
+        <h2 class="title">Essential</h2>
+        <h3 class="subtitle">Free</h3>
         <ul>
           <li><i class="pi pi-check"></i> Basic smart calendar</li>
           <li><i class="pi pi-check"></i> Client booking page</li>
@@ -39,12 +55,12 @@ function changePlan(plan) {
           <li><i class="pi pi-times"></i> Custom booking link</li>
           <li><i class="pi pi-times"></i> Advanced analytics</li>
         </ul>
-        <Button label="Choose" class="p-button-sm" />
+        <Button label="Choose" class="choose-btn">Choose</Button>
       </div>
 
       <div class="plan-card prostyle" @click="changePlan('ProStyle')">
-        <h4>ProStyle</h4>
-        <p>s/69.90 x month</p>
+        <h2 class="title">ProStyle</h2>
+        <h3 class="subtitle">s/69.90 x month</h3>
         <ul>
           <li><i class="pi pi-check"></i> Basic smart calendar</li>
           <li><i class="pi pi-check"></i> Client booking page</li>
@@ -53,12 +69,12 @@ function changePlan(plan) {
           <li><i class="pi pi-times"></i> Custom booking link</li>
           <li><i class="pi pi-times"></i> Advanced analytics</li>
         </ul>
-        <Button label="Choose" class="p-button-sm" />
+        <Button label="Choose" class="choose-btn">Choose</Button>
       </div>
 
       <div class="plan-card deluxe-studio" @click="changePlan('Deluxe Studio')">
-        <h4>Deluxe Studio</h4>
-        <p>s/129.90 x month</p>
+        <h2 class="title">Deluxe Studio</h2>
+        <h3 class="subtitle">s/129.90 x month</h3>
         <ul>
           <li><i class="pi pi-check"></i> Basic smart calendar</li>
           <li><i class="pi pi-check"></i> Client booking page</li>
@@ -67,18 +83,39 @@ function changePlan(plan) {
           <li><i class="pi pi-check"></i> Custom booking link</li>
           <li><i class="pi pi-check"></i> Advanced analytics</li>
         </ul>
-        <Button label="Choose" class="p-button-sm" />
+        <Button label="Choose" class="choose-btn">Choose</Button>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+
+.subheader {
+  padding-left: 3rem;
+  text-align: left;
+}
+.header {
+  padding-left: 3rem;
+  text-align: left;
+}
+
+.title{
+  text-align: center;
+  font-size: 1.4rem;
+  font-weight: 700;
+}
+.subtitle{
+  text-align: center;
+  font-size: 1rem;
+  margin-top: 4px;
+  color: #2A2C53;
+}
 .plans {
   display: flex;
   gap: 1rem;
   flex-wrap: wrap;
-  text-align: center;
+  justify-content: center;
 }
 
 .plan-card {
@@ -93,11 +130,20 @@ function changePlan(plan) {
 
 .plan-card:hover {
   transform: scale(1.02);
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  .choose-btn {
+    background-color: #927EA8;
+  }
 }
 
 .plan-container {
-  font-weight: bold;
-  color: #2196F3;
+  font-size: medium;
+  color: #FFFFFF;
+  background-color: #7D6498; /* Color de fondo */
+  border-radius: 10px; /* Radio para las esquinas redondeadas */
+  padding: 10px; /* Espacio entre el texto y el borde */
+  display: inline-block; /* Permite que el fondo rodee el texto */
 }
 
 ul {
@@ -110,6 +156,27 @@ li {
   display: flex;
   align-items: center;
   gap: 0.5rem;
+}
+
+.choose-btn {
+  margin-top: 2rem;
+  padding: 0.6rem 1.2rem;
+  background-color: #B6AFB9;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+}
+.choose-btn:hover {
+  background-color: #927EA8;
+}
+
+.pi-check{
+  color: #61A679;
+}
+.pi-times{
+  color: #858786;
 }
 
 .essential { background-color: #f9f9f9; }
