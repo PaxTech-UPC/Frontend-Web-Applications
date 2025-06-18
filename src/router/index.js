0ca85c08348b9e188a9bf1d7c26ec26baed6b141
Gael Rs <gaelrszk@gmail.com>
2025-06-18 11:47:02 -0500

    feat: add deploy files

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 // IAM (auth)
 
@@ -49,6 +49,8 @@ const routes = [
 
         ]
     },
+
+
 
     // Provider Layout
 
@@ -108,8 +110,8 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory('/Frontend-Web-Applications/'),
-    routes
+    history: createWebHistory('/'),
+    routes: routes,
 })
 
 export default router
